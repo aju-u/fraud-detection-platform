22edeefffd5cdf0d3fafe5798e1a2ff7afc3b25a
Juhi <juhi.shah@ada.ac.uk>
2026-09-03 19:57:48 +0100

    transactions table added

--- Frontend/src/pages/AlertsDashboard.tsx
+++ Frontend/src/pages/AlertsDashboard.tsx
@@ -9,6 +9,38 @@ export default function AlertsDashboard({ onBack, }: { onBack: () => void }) {
       </button>
 
       <h1 className="text-xl font-semibold mb-6">Alerts Dashboard</h1>
+      <table id ="simple-board">
+        <tbody>
+          <tr id="row0">
+            <th>Transaction</th>
+            <th>Customer</th>
+            <th>Merchant</th>
+            <th>Amount</th>
+            <th>Risk</th>
+          </tr>
+          <tr id="row1">
+            <td>TXN1</td>
+            <td>CUST1</td>
+            <td>MERCHANT1</td>
+            <td>£100</td>
+            <td>High</td>
+          </tr>
+          <tr id="row2">
+            <td>TXN2</td>
+            <td>CUST2</td>
+            <td>MERCHANT2</td>
+            <td>£10</td>
+            <td>Low</td>
+          </tr>
+          <tr id="row3">
+            <td>TXN3</td>
+            <td>CUST3</td>
+            <td>MERCHANT3</td>
+            <td>£70</td>
+            <td>Medium</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
